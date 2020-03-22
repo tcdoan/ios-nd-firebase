ea40161196b14987272bf6f5643c94e19d07728f
Anna Zislina <annzislina@gmail.com>
2020-03-22 20:54:24 +0200

    Migration to SWIFT 5

--- FriendlyChat/FriendlyChatSwift/AppDelegate.swift
+++ FriendlyChat/FriendlyChatSwift/AppDelegate.swift
@@ -16,7 +16,7 @@
 
 import UIKit
 import Firebase
-import FirebaseAuthUI
+import FirebaseUI
 
 // MARK: - AppDelegate: UIResponder, UIApplicationDelegate
 
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: UIApplicationDelegate
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         // TODO: - configure FIRApp
         return true
     }
